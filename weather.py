21e29a32efeb3350cbbc070fb8d41bf05a3fdf98
Harivikash M <30831591+Harivikash@users.noreply.github.com>
2024-03-08 23:16:12 +0530

    Update weather.py
    api key masking

--- weather.py
+++ weather.py
@@ -12,7 +12,7 @@ def get_weather_data():
     get_loc=get_location()
     latitude=get_loc[0]
     longitude=get_loc[1]
-    api_key = '540300516678450432c4b06efd0b07df'
+    api_key = 'xxxxxxxxxxxxxxxxxxxx'
     weather_api_url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric'
     response = requests.get(weather_api_url)
     print(latitude, longitude)
